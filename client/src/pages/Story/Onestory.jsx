11eb6b2c004d374df416737d27ffbc31563fa0ed
buzapeter12345 <buzapeter12345@gmail.com>
2023-11-21 13:23:40 +0100

    rating

--- client/src/pages/Story/Onestory.jsx
+++ client/src/pages/Story/Onestory.jsx
@@ -21,6 +21,7 @@ const Onestory = () => {
   const [kategoria, setKategoria] = useState("");
   const [story, SetStory] = useState("");
   const [hozzaszolas, SetHozzaszolas] = useState("");
+  const [sajatErtekeles, SetSajatErtekeles] = useState("");
 
   const userLocalStorage = JSON.parse(localStorage.getItem("user"));
   const token = userLocalStorage.token;
@@ -115,6 +116,84 @@ const Onestory = () => {
             <h3>{szerzo}</h3>
           </Link>
           <p>{karakterek}</p>
+          <p>{sajatErtekeles}⭐</p>
+          <div className="rating">
+            <input type="radio" id="star-1" name="star-radio" value="star-1" />
+            <label for="star-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                onClick={() => {
+                  SetSajatErtekeles(5);
+                }}
+              >
+                <path
+                  pathLength="360"
+                  d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
+                ></path>
+              </svg>
+            </label>
+            <input type="radio" id="star-2" name="star-radio" value="star-1" />
+            <label for="star-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                onClick={() => {
+                  SetSajatErtekeles(4);
+                }}
+              >
+                <path
+                  pathLength="360"
+                  d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
+                ></path>
+              </svg>
+            </label>
+            <input type="radio" id="star-3" name="star-radio" value="star-1" />
+            <label for="star-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                onClick={() => {
+                  SetSajatErtekeles(3);
+                }}
+              >
+                <path
+                  pathLength="360"
+                  d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
+                ></path>
+              </svg>
+            </label>
+            <input type="radio" id="star-4" name="star-radio" value="star-1" />
+            <label for="star-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                onClick={() => {
+                  SetSajatErtekeles(2);
+                }}
+              >
+                <path
+                  pathLength="360"
+                  d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
+                ></path>
+              </svg>
+            </label>
+            <input type="radio" id="star-5" name="star-radio" value="star-1" />
+            <label for="star-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                onClick={() => {
+                  SetSajatErtekeles(1);
+                }}
+              >
+                <path
+                  pathLength="360"
+                  d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
+                ></path>
+              </svg>
+            </label>
+          </div>
         </div>
         <div className="onlystory">
           <h1>{cim}</h1>
