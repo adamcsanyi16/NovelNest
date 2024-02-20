be75ef27e8ca073e548e8db3a163cf51001c8ac3
buzapeter12345 <buzapeter12345@gmail.com>
2024-02-20 10:01:35 +0100

    NotFound

--- client/src/pages/Notfound/Notfound.jsx
+++ client/src/pages/Notfound/Notfound.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Notfound = () => {
   return (
-    <div>Notfound</div>
-  )
-}
+    <div className="not-found">
+      <div className="keret">
+        <img
+          className="lany-kep"
+          src="/images/merges-lany.png"
+          alt="lany-kep"
+        />
+        <p className="egyszer1">Egyszer</p>
+        <p className="volt1">volt</p>
+        <p className="hol">hol</p>
+        <p className="nem">nem</p>
+        <p className="volt2">volt.</p>
+        <p className="volt3">Volt</p>
+      </div>
+      <div className="keret2">
+        <p className="page-not">PAGE NOT FOUND</p>
+        <div className="utolso">
+          <p className="egyszer2">egyszer</p>
+          <p className="egy">egy...</p>
+        </div>
+        <div className="gomb">
+          <Link to={"/"}>
+            <button className="vissza">Vissza a főoldalra!</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Notfound
+export default Notfound;
